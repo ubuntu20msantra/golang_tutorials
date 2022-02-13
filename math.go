2d4647ebd1d90cd0533b157615e144fa6af7e39b
manas-pc <msantra.ubuntu20@gmail.com>
2022-02-13 00:28:26 -0800

    mathematical ops

---
+++ math.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 9
+	b := 3
+
+	sum := a + b
+	sub := a - b
+	mul := a * b
+	div := a / b
+
+	fmt.Println("Addition of two numbers : ", sum)
+	fmt.Println("Substraction of two numbers : ", sub)
+	fmt.Println("Multiplication of two numbers : ", mul)
+	fmt.Println("Devision of two numbers : ", div)
+
+	// Addition of two numbers :  12
+	// Substraction of two numbers :  6
+	// Multiplication of two numbers :  27
+	// Devision of two numbers :  3
+}
